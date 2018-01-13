f555558f1d630c07c0ee8774e141f9aef2ee3e1f
unknown <info@alecreative.com>
2018-01-13 11:23:07 +0100

    First commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+use Medoo\Medoo;
+require_once '../vendor/autoload.php';
+echo "Hello World";
+$database = new Medoo([
+    'database_type' => 'sqlite',
+    'database_file' => '../storage/database.db'
+]);
+dump($database);
